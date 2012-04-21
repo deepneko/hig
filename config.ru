ec425c74d4d6e8218308b2313852243b0015d5e5
deepneko <deepneko@softbank126101147058.bbtec.net>
2012-04-21 17:59:59 +0900

    20120421

--- config.ru
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require './hishow'
-require 'sinatra'
-run Sinatra::Application
---
+++ hig.rb
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'cinch'
+require 'socket'
+require 'slop'
+
+opts = Slop.parse do
+  on :s, :irc_host=, '127.0.0.1'
+  on :p, :irc_port=, 16667
+  on :n, :nickname=, "hishow_"
+  on :c, :channel=, "#hixi-test"
+  on :l, :local_port=, 4444
+  on :h, :remote_host=, '127.0.0.1'
+  on :r, :remote_port=, 4445
+end
+
+client = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+c_sockaddr = Socket.sockaddr_in(opts[:remote_port].to_i, opts[:remote_host])
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = opts[:irc_host]
+    c.port = opts[:irc_port].to_i
+    c.nick = opts[:nickname]
+    c.realname = opts[:nickname]
+    c.user = opts[:nickname]
+    c.channels = [opts[:channel]]
+  end
+  
+  on :message do |m|
+    unless m.user == opts[:nickname]
+      buf = "(#{m.user}) #{m.message}"
+      client.puts(buf)
+    end
+  end
+end
+
+t = Thread.new do
+  begin
+    p "try connect"
+    client.connect(c_sockaddr)
+  rescue Errno::ECONNREFUSED
+    sleep 30
+    client.close
+    client = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+    c_sockaddr = Socket.sockaddr_in(opts[:remote_port].to_i, opts[:remote_host])
+    retry
+  end
+  bot.start
+end
+t.run
+
+server = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+sockaddr = Socket.sockaddr_in(opts[:local_port].to_i, "127.0.0.1")
+server.bind(sockaddr)
+server.listen(5)
+while true
+  sock, sockaddr = server.accept
+
+  while buf = sock.gets
+    bot.irc.send("PRIVMSG #{opts[:channel]} :#{buf}")
+  end
+
+  sock.close
+end
